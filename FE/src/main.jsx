c76bc34082aaae09c1657c18af6c53ecbb2f7bdb
d4ngv0 <24520259@gm.uit.edu.vn>
2025-01-10 23:29:22 +0700

    add tailwind. add Detail.jsx, BreadCrumb.jsx. modified index.html for display icons. App.jsx, main.jsx modified for testing. add Show.jsx, Header.jsx, ProductDetail.jsx for testing.

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Show from './client/product/Show.jsx'
+import Detail from './client/product/Detail.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+      </Route>
+
+      <Route path="products">
+          <Route index element={<Show />} />
+          <Route path=":pid" element={<Detail />} />
+      </Route> 
+      
+    </Routes>
+    
+  </BrowserRouter>
+ 
 )
---
+++ FE/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    
+      "./index.html",
+      "./src/App.jsx",
+      "./src/client/product/Detail.jsx",
+      "./src/client/header/Header.jsx",
+      "./src/client/header/BreadCrumb/BreadCrumb.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
