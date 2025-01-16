8b3fce496988fa89ea6bbcf64d897ae23bbf2786
LanTran2006 <lantrancute2006@gmail.com>
2025-01-16 18:50:31 +0700

    update login/register

--- FE/src/client/Login/Login.jsx
+++ FE/src/client/Login/Login.jsx
@@ -1,117 +1,85 @@
 import React, { useState } from "react";
-import "./Login.css";
+import "./Login.css"
+import RegisterPage from "../Register/Register";
 
-const initFormValue = {
-  username: "",
-  password: "",
-};
-
-const isEmptyValue = (value) => {
-  return !value || value.trim().length < 1;
-};
-const isEmail = (email) => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-};
-export default function LoginPage() {
-  const [formValue, setFormValue] = useState(initFormValue);
-  const [formError] = useState({});
-
-  const validate = () => {
-    const error = {};
-    if (isEmptyValue(formValue.username)) {
-      error["username"] = "Username is required!";
-    } else {
-      if (isEmail(formValue.username)) {
-        error["username"] = "Email is invalid!";
-      }
-    }
-    if (isEmptyValue(formValue.password)) {
-      error["Password"] = "Password is required!";
-    }
-
-    return Object.keys(error).length === 0;
-  };
-
-  const handleChange = (event) => {
-    const { value, name } = event.target;
-    setFormValue({
-      ...formValue,
-      [name]: value,
+export default function LoginPage()
+{
+    const [form, setForm] = useState({
+            username:'',
+            password:'',
+            
+        });
+    
+    const [error,setError] = useState({
+            username:'',
+            password:'',
+            
     });
-  };
+    const handleChange=(event)=>
+    {
+            setForm({...form,[event.target.id]:event.target.value});
+    };
+    const handleSubmit = (event) =>
+        {
+            event.preventDefault();
+            const newError ={};
+            if (form.username.length === 0 )
+            {
+                newError.username="Username must not be empty";
+            }
+            else if ( form.username.length > 20 )
+            {
+                newError.username="Username must not have over 20 characters";
+            }
+            else if (!/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/.test(form.username))
+            {
+                newError.username="Username must be an email";
+            }
+            else{
+                newError.username="";
+            }
+    
+            if (form.password.length === 0 )
+            {
+                
+                    newError.password="Password must not be empty";
+            }
+            else if ( form.password.length > 20 || form.password.length < 5 )
+            {
+                    newError.password="Password must not have over 20 characters and at least 5 characters";
+                    
+            }
+            else{
+                newError.password="";
+            }
+    
+            if ( Object.keys(newError).length >  0)
+            {
+                    
+                    setError({ ...newError });
+            }
+            else{
+                alert("Đăng nhập thành công!");
+            }
+        }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+       
 
-    if (validate()) {
-      console.log("formValue", formValue);
-    } else {
-      console.log("form invalid");
-    }
-    console.log("formValue", formValue);
-  };
-
-  return (
-    <div className="LoginPage">
-      <div className="Header">
-        <img src="logo.png" alt="Logo"></img>
-        <p>Login Page</p>
-      </div>
-
-      <div className="Body">
-        <div className="row content">
-          <div className="col-6"></div>
-          <div className="col-6 Form ">
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="dangnhap" className="dangnhap">
-                Đăng nhập
-              </label>
-              <br></br>
-              <label
-                htmlFor="username"
-                style={{ paddingRight: "70%", fontSize: "30px" }}
-              >
-                Username
-              </label>
-              <br></br>
-              <input
-                type="text"
-                placeholder="Enter your email"
-                name="username"
-                className="input_username"
-                value={formValue.username}
-                onChange={handleChange}
-              ></input>
-              {formError.username && (
-                <div className="error-feedback">{formError.username}</div>
-              )}
-              <br></br>
-              <label
-                htmlFor="password"
-                style={{ paddingRight: "70%", fontSize: "30px" }}
-              >
-                Password
-              </label>
-              <br></br>
-              <input
-                type="password"
-                name="password"
-                id=""
-                placeholder="Enter your password"
-                className="input_password"
-                value={formValue.password}
-                onChange={handleChange}
-              ></input>
-              <button className="btn_dangnhap">ĐĂNG NHẬP</button>
-              <br></br>
-              <a href="google.com" className="quenmatkhau">
-                Quên Mật Khẩu
-              </a>
-              <a href="google.com"> Đăng Ký </a>
-            </form>
-          </div>
+    return(
+        <div className="main">
+                <form className="form" onSubmit={handleSubmit}>
+                    <h2 className="mb-4">Đăng Nhập</h2>
+                    <input type="email" placeholder="Enter your email here" className="mb-4 w-100 h-25 p-2 border-3 border-warning-subtle rounded-3" onChange={handleChange}></input>
+                    {error.username && (<p className="error">{error.username}</p>)}
+                    <br></br>
+                    <input type="password" placeholder="Enter your password here" className="mb-4 p-2 w-100 h-50 border-3 border-warning rounded-3" onChange={handleChange}></input>
+                    {error.password && (<p className="error">{error.password}</p>)}
+                    <br></br>
+                    <button type="submit" className="mb-4 w-100 h-25 border-3 rounded-3 p-2">Đăng nhập</button>
+                    <p>Bạn chưa có tài khoản?<a href="#" >Đăng ký ngay</a></p>
+                </form>
         </div>
-      </div>
-    </div>
-  );
+    );
+        
 }
+
