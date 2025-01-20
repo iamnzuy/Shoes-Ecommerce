2ec21ddc31d2858cb75ce8ac2d1a5f8d7a3fcd78
Liyued <leduy1212123@gmail.com>
2025-01-20 19:09:02 +0700

    add swiper for slideshow

--- FE/src/client/home/Slide/ProductSlideshow.jsx
+++ FE/src/client/home/Slide/ProductSlideshow.jsx
@@ -1,57 +1,36 @@
 import { useState, useEffect } from "react";
 import ProductCard from "../../product/ProductCard";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
 
 function ProductSlideshow(props) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  // Function to go to the next image
-  const nextImage = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === Math.ceil(props.products.length / 3) - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const prevImage = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? Math.ceil(props.products.length / 3) - 1 : prevIndex - 1
-    );
-  };
-
-  useEffect(() => {
-    const interval = setInterval(nextImage, 4000);
-    return () => clearInterval(interval); // Cleanup on component unmount
-  }, []);
-
+  const swiper = useSwiper();
+  const [swiperInstance, setSwiperInstance] = useState(null);
   return (
-    <div className="relative w-full max-w-6xl mx-auto overflow-hidden my-12">
-      <div
-        className="flex transition-transform duration-500"
-        style={{
-          transform: `translateX(-${currentIndex * 100}%)`,
-        }}
+    <div className="relative w-full max-w-8xl mx-auto overflow-hidden my-12">
+      <Swiper
+        onSwiper={(swiper) => setSwiperInstance(swiper)}
+        slidesPerView={3}
       >
         {props.products.map((product, index) => (
-          <div key={index}>
+          <SwiperSlide key={index}>
             <ProductCard
-              name={product.name}
-              price={product.price}
               image={product.image}
-              id={product._id}
-              full="1"
+              price={product.price}
+              name={product.name}
             />
-          </div>
+          </SwiperSlide>
         ))}
-      </div>
-      {/* Left Button */}
+      </Swiper>
       <button
-        onClick={prevImage}
+        onClick={() => swiperInstance?.slidePrev()}
         className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full z-10"
       >
         &lt;
       </button>
       {/* Right Button */}
       <button
-        onClick={nextImage}
+        onClick={() => swiperInstance?.slideNext()}
         className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full z-10"
       >
         &gt;
--- FE/src/client/product/ProductCard.jsx
+++ FE/src/client/product/ProductCard.jsx
@@ -10,7 +10,7 @@ function ProductCard(props) {
     >
       <img
         src={props.image}
-        className="w-64 h-64 transition-all duration-500 group-hover:scale-110"
+        className="w-64 h-64 transition-all duration-500 group-hover:scale-110 rounded-md"
       ></img>
       <p className="text-xl font-bold my-4 group-hover:text-red-600 transition-colors duration-500">
         {props.name}
