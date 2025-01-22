976b2422d6bd499a5a606ede66f099ad26d3d854
Phan Chi Kien <147979609+pckienuit@users.noreply.github.com>
2025-01-22 17:22:19 +0700

    Fix: "Go to home" not work

--- FE/src/components/404_notfound/404_notfound.jsx
+++ FE/src/components/404_notfound/404_notfound.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './404_notfound.css';
+import React from "react";
+import "./404_notfound.css";
 
-function NotFound () {
+function NotFound() {
     return (
         <>
             <div className="col-flex-container-80">
@@ -9,21 +9,17 @@ function NotFound () {
                     <div id="error-contents" className="col-flex-container">
                         <div id="main-text">
                             <div className="row-flex-container">
-                                4<div id="bracket">{'{}'}</div>4
+                                4<div id="bracket">{"{}"}</div>4
                             </div>
                         </div>
-                        <h1>
-                            Something is missing
-                        </h1>
+                        <h1>Something is missing</h1>
                         <p>The page you looking for is not found.</p>
                         <button id="cart-checkout-btn">
                             <div className="row-flex-container-100">
-                                Go to home
+                                <a href="/">Go to home</a>
                             </div>
                         </button>
-                        <p id="or">
-                            - OR -
-                        </p>
+                        <p id="or">- OR -</p>
                         <div id="dosomething">
                             <div className="row-flex-container">
                                 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
